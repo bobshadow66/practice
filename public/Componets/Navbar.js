4b5d32fbdbfca54eee4f11cdf6d8b4b7f6cdf31e
bobshadow66 <eebooby@gmail.com>
2024-07-20 11:34:16 -0700

    Added: Icon Button, Text, and other stuff

--- public/Componets/Navbar.js
+++ public/Componets/Navbar.js
@@ -1,48 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../Assets/Logo.svg";
-import { BsCart2 } from "react-icons/bd";
-import {HiOutlineBars3} from "react-icons/h2";
-import { 
-    Box, 
-    Drawer, 
-    ListItem, 
-    ListenItemButton, 
-    ListItemIcon, 
-    ListItemText,
-} from "@mui/matieral";
-import {HomeIcon, InfoIcon} from "@mui/icons-materials/Home";
-import infoIcon from "@mui/icons-materials/Info";
-import CommentRoundedIcon from "@mui/icons-materials/commentRounded";
-import PhoneRoundedIcon from "@mui/icons-materials/PhoneRounded";
-import ShoppingCartRoundedIcon from "@mui/icons-materials/ShoppingCartRounded";
+import { BsCart2 } from "react-icons/bs";
+import { HiOutlineBars3 } from "react-icons/hi";
+import {
+  Box,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
 
 const Navbar = () => {
-    
-    const [openMenu,setOpenMenu] = useState(false)
-    const menuOptions = [
-       {
-        text: "Home",
-        icon: <HomeIcon/>
-       }, 
-       {
-        text: "About",
-        icon: <InfoIcon/>
-       }, 
-       {
-        text: "Testimonials",
-        icon: <CommentRoundedIcon/>
-       }, 
-       {
-        text: "Contact",
-        icon: <PhoneRoundedIcon/>
-       }, 
-       {
-        text: "Cart",
-        icon: <ShoppingCartRoundedIcon/>
-       }, 
-    ];
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuOptions = [
+    {
+      text: "Home",
+      icon: <HomeIcon />,
+    },
+    {
+      text: "About",
+      icon: <InfoIcon />,
+    },
+    {
+      text: "Testimonials",
+      icon: <CommentRoundedIcon />,
+    },
+    {
+      text: "Contact",
+      icon: <PhoneRoundedIcon />,
+    },
+    {
+      text: "Cart",
+      icon: <ShoppingCartRoundedIcon />,
+    },
+  ];
 
-    return <nav></nav>;
+  return (
+    <nav>
+      <div className="nav-logo-container">
+        <img src={Logo} alt="Logo" />
+      </div>
+      <div className="navbar-links-container">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#testimonials">Testimonials</a>
+        <a href="#contact">Contact</a>
+        <a href="#cart">
+          <BsCart2 className="navbar-cart-icon" />
+        </a>
+        <button className="primary-button">Book Now</button>
+      </div>
+      <div className="navbar-menu-container">
+        <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+      </div>
+      <Drawer
+        open={openMenu}
+        onClose={() => setOpenMenu(false)}
+        anchor="right"
+      >
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={() => setOpenMenu(false)}
+          onKeyDown={() => setOpenMenu(false)}
+        >
+          <List>
+            {menuOptions.map((item) => (
+              <ListItem key={item.text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText primary={item.text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
+    </nav>
+  );
 };
 
 export default Navbar;
