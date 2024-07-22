709149e27008c57f29555a7b4bf57df9a780200d
bobshadow66 <eebooby@gmail.com>
2024-07-22 14:00:24 -0700

    Added: two new images
    Modified: Home.js and App.js to include the images

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,35 +1,35 @@
 import React from 'react';
-//import Navbar from "./Navbar.js";
-import BannerBackground from "../Assets/home-banner-background.png"
-import BannerImage from "../Assets/home-banner-image.png"
-import {FiArrowRight} from "react-icons/fi";
+// import Navbar from "./Navbar.js";
+import BannerBackground from "../Assets/home-banner-background.png";
+import BannerImage from "../Assets/home-banner-image.png";
+import { FiArrowRight } from "react-icons/fi";
 
 const Home = () => {
-    // javascript here.
-    return (
-      <div className="home-container">
-      {/*<Navbar />*/}
-        <div className="home-banner-container" ></div>
-        <div className="home-bannerImage-container" >
-            <img src={BannerBackground} alt="" />
+  return (
+    <div className="home-container">
+      {/* <Navbar /> */}
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="Banner Background" />
         </div>
         <div className="home-text-section">
-            <h1 className="primary-heading" >
-              Your Favourite Food Delivered Hot & Fresh
-            </h1>
-            <p className="primary-text">
-              Healthy switcher chefs do all the prep work, like peeding, chopping
-              & marinating, so you can cook fresh food
-            </p>
-            <button className="secondary-button">
-              Order Now <FiArrowRight />
-            </button>
+          <h1 className="primary-heading">
+            Your Favourite Food Delivered Hot & Fresh
+          </h1>
+          <p className="primary-text">
+            Healthy switcher chefs do all the prep work, like peeling, chopping
+            & marinating, so you can cook fresh food.
+          </p>
+          <button className="secondary-button">
+            Order Now <FiArrowRight />
+          </button>
         </div>
-        <div className="home-image-container" >
-      <img src="{BannerImage" alt="" />
+        <div className="home-image-container">
+          <img src={BannerImage} alt="Banner" />
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Home;
